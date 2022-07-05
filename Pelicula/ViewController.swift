cb94cab44864f35ceabb4023c07dca0601ad3cef
mcor <mcor@10-48-48-108.0200.ufpe.br.iosda.org>
2022-07-05 14:05:47 -0300

    Adiciona projeto ao repositório

---
+++ Pelicula/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pelicula
+//
+//  Created by mcor on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
